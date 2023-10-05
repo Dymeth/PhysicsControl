0e794eece247157dac22e4b4ccd36259e6a9ddca
Danila Mitrofanov <DymethInfo@gmail.com>
2023-10-05 16:52:28 +0700

    Ignore legacy materials in MaterialKeysSet

--- Api/src/main/java/ru/dymeth/pcontrol/api/set/MaterialKeysSet.java
+++ Api/src/main/java/ru/dymeth/pcontrol/api/set/MaterialKeysSet.java
@@ -4,10 +4,17 @@ import org.bukkit.Material;
 import ru.dymeth.pcontrol.api.BukkitUtils;
 
 import javax.annotation.Nonnull;
+import java.util.function.Predicate;
 
 public final class MaterialKeysSet extends EnumKeysSet<Material> {
     public MaterialKeysSet(@Nonnull Material... elements) {
         super(Material.class, elements);
+    }
+
+    @Nonnull
+    @Override
+    public EnumKeysSet<Material> add(@Nonnull Predicate<Material> filter) {
+        return super.add(material -> filter.test(material) && !material.name().startsWith("LEGACY_"));
     }
 
     @Nonnull
